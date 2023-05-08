b93c5a5b756e637809fa585cb01047b2b090a74c
bettoJogar <bettoJogar@gmail.com>
2023-05-08 15:24:50 -0400

    Al

--- Classes/Crud.class.php
+++ Classes/Crud.class.php
@@ -4,15 +4,10 @@
         abstract function inserir();
         abstract function atualizar($campo, $id);
 
-<<<<<<< HEAD
+
         public function listar( $where = null){
             $sqlSelect = "select * from {$this->tabela} $where";
             return Conexao::query($sqlSelect);
-=======
-        public function listar(){
-            $selctSql = "SELECT * FROM {$this->tabela}";
-            return Conexao::query($selctSql);
->>>>>>> e5867cb265b4b8d304689c1f3ff0ac5584563269
         }
         public function buscar($campo, $id){
             $selctSql = "SELECT * FROM {$this->tabela} WHERE $campo = {$id}";
