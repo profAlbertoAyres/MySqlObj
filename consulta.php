f12506e31dc7398e35c5b0cbc8198f6ee72b2434
bettoJogar <bettoJogar@gmail.com>
2023-05-15 15:41:03 -0400

    Atualização 15/05

--- consulta.php
+++ consulta.php
@@ -1,3 +1,20 @@
+<?php
+spl_autoload_register(function ($class) {
+    require_once "./Classes/{$class}.class.php";
+});
+if (filter_has_var(INPUT_POST, 'pacienteCon')) {
+    $idPac = filter_input(INPUT_POST, 'pacienteCon');
+} else {
+?>
+    <script>
+        alert("Nenhum paciente selecionado");
+        window.location.href = "index.php"
+    </script>
+<?php
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -8,7 +25,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="css/layout.css">
-    <title>Pacientes</title>
+    <title>Consultas de </title>
 </head>
 
 <body>
@@ -44,48 +61,28 @@
     <main class="mt-3">
         <div class="container mt-3">
             <div class="d-flex flex-row-reverse">
-                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="col-md-6">
-                    <div class="input-group mb-3">
-                        <div class="form-floating">
-                            <input type="text" class="form-control" id="txtPesquisar" placeholder="Pesquisar" name="txtPesquisar">
-                            <label for="pesquisar">Pesquisar</label>
-                        </div>
-                        <button class="btn btn-outline-secondary" type="submit" id="btnPesquisar" name="btnPesquisar">
-                            <span class="material-symbols-outlined">
-                                search
-                            </span>
-                        </button>
-                    </div>
-                </form>
+                <a href="consultaGer.php" class="btn btn-info">Nova Consulta</a>
             </div>
+            
             <table class="table mt-3">
                 <thead class="table-dark">
                     <tr>
-                        <th scope="col">Açoẽs</th>
-                        <th scope="col">Foto</th>
-                        <th scope="col">Nome</th>
-                        <th scope="col">E-mail</th>
-                        <th scope="col">Celular</th>
+                        <!-- <th scope="col">Açoẽs</th> -->
+                        <th scope="col">Medico</th>
+                        <th scope="col">Data</th>
+                        <th scope="col">Hora</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    spl_autoload_register(function ($class) {
-                        require_once "./Classes/{$class}.class.php";
-                    });
-                    $paciente = new Paciente();
-                    if (filter_has_var(INPUT_POST, 'txtPesquisar')) {
-                        $parametro = filter_input(INPUT_POST, 'txtPesquisar');
-                        $where = "where (nomePac like '%$parametro%' ) or (emailPac like '%$parametro%' )";
-                        $dadosBanco =  $paciente->listar($where);
-                    } else {
-                        $dadosBanco =  $paciente->listar();
-                    }
+                    $consulta = new Consulta;
+                    $where =  "where C.pacienteCon = $idPac";
+                    $dadosBanco = $consulta->listar($where);
                     while ($row = $dadosBanco->fetch_object()) {
                     ?>
                         <tr>
-                            <td class="align-middle" scope="row">
-                                <a href="pacienteGer.php?id=<?php echo $row->idPac ?>" class="btn btn-secondary">
+                            <!-- <td class="align-middle" scope="row">
+                                <a href="pacienteGer.php?id=<?php echo $row->idCon ?>" class="btn btn-secondary">
                                     <span class="material-symbols-outlined">
                                         edit_square
                                     </span>
@@ -95,28 +92,18 @@
                                         delete
                                     </span>
                                 </a>
-                                <form action="consulta.php" method="post" style="display: inline-block;">
-                                <input type="hidden" name="pacienteCon" value="<?php echo $row->idPac ?>">
-                                <button type="submit" class="btn btn-success">
-                                <span class="material-symbols-outlined">
-                                        description
-                                    </span>
-                                </button>    
-                                </form>
-                            </td>
-                            <td class="align-middle">
-                                <img src="imagesPac/<?php echo $row->fotoPac ?>" alt="Foto do paciente <?php echo $row->nomePac ?>" class="imgRed">
-                            </td>
-                            <td class="align-middle"><?php echo $row->nomePac ?></td>
-                            <td class="align-middle"><?php echo $row->emailPac ?></td>
-                            <td class="align-middle"><?php echo $row->celularPac ?></td>
+                            </td> -->
+                            <td class="align-middle"><?php echo $row->nomeMed ?></td>
+                            <td class="align-middle"><?php echo $row->dataCon ?></td>
+                            <td class="align-middle"><?php echo $row->horaCon ?></td>
                         </tr>
                     <?php } ?>
                 </tbody>
+
             </table>
         </div>
     </main>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
---
+++ consultaGer.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Médico</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Clinica IFRO</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="pacientes.php">Paciente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Médico</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="especialidades.php">Especialidade</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link">Consultas</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container mt-3">
+            <?php
+            spl_autoload_register(function ($class) {
+                require_once "./Classes/{$class}.class.php";
+            });
+            if (filter_has_var(INPUT_GET, 'id')) {
+                $consulta = new Consulta();
+                $id = filter_input(INPUT_GET, 'id');
+                $conEdit = $consulta->buscar('idCon', $id);
+            }
+            if (filter_has_var(INPUT_GET, 'idDel')) {
+                $consulta = new Consulta();
+                $id = filter_input(INPUT_GET, 'idDel');
+                $consulta->deletar('idCon', $id);
+            ?>
+                <script>
+                    window.location.href = 'consultas.php';
+                </script>
+            <?php
+            }
+            if (filter_has_var(INPUT_POST, 'btnGravar')) {
+                $consulta = new Consulta();
+                $id = filter_input(INPUT_POST, 'txtCodigo');
+                $consulta->setIdCon($id);
+                $consulta->setMedicoCon(filter_input(INPUT_POST,'sltMedico'));
+                $consulta->setPacienteCon(filter_input(INPUT_POST, 'sltPaciente'));
+                $consulta->setDataCon(filter_input(INPUT_POST, 'txtData'));
+                $consulta->setHoraCon(filter_input(INPUT_POST, 'txtHora'));
+
+                if (empty($id)) {
+                    $consulta->inserir();
+                } else {
+                    $consulta->atualizar('idCon', $id);
+                }
+            }
+
+            ?>
+            <form class="row g-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="txtCodigo" value="<?php echo isset($conEdit->idCon) ? $conEdit->idCon : null; ?>">
+                <div class="col-md-6">
+                    <label for="sltPaciente" class="form-label">Paciente</label>
+                    <select id="sltPaciente" class="form-select" name="sltPaciente">
+                        <?php $pacSel = isset($conEdit->pacienteCon) ? $conEdit->pacienteCon : null; ?>
+                        <option value="" selected hidden>Escolha...</option>
+                        <?php 
+                        
+                        $paciente = new Paciente();
+                        $dadosBanco =  $paciente->listar();
+                        while ($row = $dadosBanco->fetch_object()) {
+                        ?>
+                        <option value="<?php echo $row->idPac ?>" <?php if ($pacSel === $row->idPac) {
+                                                echo 'selected';
+                                            } ?>><?php echo $row->nomePac ?></option>
+                                            <?php }?>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="sltMedico" class="form-label">Especialidade</label>
+                    <select id="sltMedico" class="form-select" name="sltMedico">
+                        <?php $medSel = isset($conEdit->medicoCon) ? $conEdit->medicoCon : null; ?>
+                        <option value="" selected hidden>Escolha...</option>
+                        <?php 
+                        
+                        $medico = new Medico;
+                        $dadosBanco =  $medico->listar();
+                        while ($row = $dadosBanco->fetch_object()) {
+                        ?>
+                        <option value="<?php echo $row->idMed ?>" <?php if ($medSel === $row->idMed) {
+                                                echo 'selected';
+                                            } ?>><?php echo $row->nomeMed ?></option>
+                                            <?php }?>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="txtData" class="form-label">Data</label>
+                    <input type="date" class="form-control" id="txtData" name="txtData" value="<?php echo isset($conEdit->dataCon) ? $conEdit->dataCon : null; ?>">
+                </div>
+                <div class="col-md-6">
+                    <label for="txtHora" class="form-label">Data</label>
+                    <input type="time" class="form-control" id="txtHora" name="txtHora" value="<?php echo isset($conEdit->horaCon) ? $conEdit->horaCon : null; ?>">
+                </div>
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary" name="btnGravar">Gravar</button>
+                </div>
+            </form>
+        </div>
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</body>
+
+</html>
